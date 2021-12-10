baac1c7f21b8f88961c31a7d7460534610dead93
Vady Mora <richard_vady1998@hotmail.com>
2021-12-10 00:45:02 -0500

    actualización teatro
    paneles teatro

--- CINE/src/CONTROLADORES/logica_negocio_panelTeatro.java
+++ CINE/src/CONTROLADORES/logica_negocio_panelTeatro.java
@@ -1,7 +1,11 @@
 package CONTROLADORES;
 
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
 import MODELO.cine;
 import MODELO.pelicula;
+import VISTA.panel_Cartelera;
 import VISTA.panel_cine;
 import VISTA.panel_teatro;
 import patron_generico_g2.configurable;
@@ -10,9 +14,16 @@ public class logica_negocio_panelTeatro implements configurable {
 	private cine ARCANE;
 	private panel_teatro pt;
 	private pelicula[] peliculas;
+	private panel_Cartelera pcc;
+	private JLabel[] auxi;
 	
+	public logica_negocio_panelTeatro(panel_teatro pc_,pelicula[] peliculas_) {
+		this.pt= pc_;
+		auxi= new JLabel[10];
+	}
 	public logica_negocio_panelTeatro(panel_teatro pc_,pelicula[] peliculas_,cine ARCANE_) {
 		this.ARCANE=ARCANE_;
+		auxi= new JLabel[10];
 		this.pt= pc_;		
 		peliculas= peliculas_;
 		cargarPeliculasComboBox();
@@ -36,6 +47,27 @@ public class logica_negocio_panelTeatro implements configurable {
 					pt.txt_censura.setText("SI");					
 				pt.txt_genero.setText(peliculas[j].getDatos().getAtributoT2());
 				pt.txtA_sinapsis.setText(peliculas[j].getDatos().getAtributoT3());
+			}
+			if(pt.txt_nombre.getText().toString().equals("La sirenita")) {
+				pt.lbl_fotos.setIcon(new ImageIcon(panel_Cartelera.class.getResource("/Iconos/Sirenita.jpeg")));
+			}else if(pt.txt_nombre.getText().toString().equals("Pinocho")) {
+				pt.lbl_fotos.setIcon(new ImageIcon(panel_Cartelera.class.getResource("/Iconos/Pinocho.jpg")));
+			}else if(pt.txt_nombre.getText().toString().equals("SAW")) {
+				pt.lbl_fotos.setIcon(new ImageIcon(panel_Cartelera.class.getResource("/Iconos/Saw.jpeg")));
+			}else if(pt.txt_nombre.getText().toString().equals("El exorcista")) {
+				pt.lbl_fotos.setIcon(new ImageIcon(panel_Cartelera.class.getResource("/Iconos/Exorcista.jpeg")));
+			}else if(pt.txt_nombre.getText().toString().equals("SPIDERMAN NO WAY HOME")) {
+				pt.lbl_fotos.setIcon(new ImageIcon(panel_Cartelera.class.getResource("/Iconos/Spiderman.jpeg")));
+			}else if(pt.txt_nombre.getText().toString().equals("La senicienta")) {
+				pt.lbl_fotos.setIcon(new ImageIcon(panel_Cartelera.class.getResource("/Iconos/Senisienta.jpeg")));
+			}else if(pt.txt_nombre.getText().toString().equals("El rito")) {
+				pt.lbl_fotos.setIcon(new ImageIcon(panel_Cartelera.class.getResource("/Iconos/Rito.jpeg")));
+			}else if(pt.txt_nombre.getText().toString().equals("Enterrada")) {
+				pt.lbl_fotos.setIcon(new ImageIcon(panel_Cartelera.class.getResource("/Iconos/enterrado.jpeg")));
+			}else if(pt.txt_nombre.getText().toString().equals("ENCHUFETV")) {
+				pt.lbl_fotos.setIcon(new ImageIcon(panel_Cartelera.class.getResource("/Iconos/Dedidaca_a_mi_ex.jpeg")));
+			}else if(pt.txt_nombre.getText().toString().equals("Vivo")) {
+				pt.lbl_fotos.setIcon(new ImageIcon(panel_Cartelera.class.getResource("/Iconos/Vivo.jpeg")));
 			}
 		}
 		
--- CINE/src/VISTA/panel_teatro.java
+++ CINE/src/VISTA/panel_teatro.java
@@ -15,18 +15,20 @@ import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
 
 public class panel_teatro extends JPanel {
 	private JPanel panel_teatro;
 	private JPanel panel_menu;
 	private JPanel panel_datos;
 	public JComboBox comboBox;
-	public JTextField txt_nombre;
-	public JTextField txt_genero;
-	public JTextField txt_censura;
-	public JTextArea txtA_sinapsis;
 	
 	private logica_negocio_panelTeatro t_pc;
+	public JTextField txt_censura;
+	public JTextArea txtA_sinapsis;
+	public JTextField txt_genero;
+	public JTextField txt_nombre;
+	public JLabel lbl_fotos;
 	
 	/**
 	 * Create the panel.
@@ -36,59 +38,15 @@ public class panel_teatro extends JPanel {
 		
 		panel_teatro = new JPanel();
 		panel_teatro.setBackground(new Color(245, 222, 179));
-		panel_teatro.setBounds(10, 11, 355, 110);
+		panel_teatro.setBounds(0, 0, 1024, 800);
 		add(panel_teatro);
 		panel_teatro.setLayout(null);
-		
-		JPanel panel_detalles = new JPanel();
-		panel_detalles.setLayout(null);
-		panel_detalles.setBounds(10, 132, 423, 296);
-		add(panel_detalles);
-		
-		JLabel lbl_nombre = new JLabel("NOMBRE");
-		lbl_nombre.setBounds(10, 28, 70, 14);
-		panel_detalles.add(lbl_nombre);
-		
-		JLabel lbl_genero = new JLabel("GENERO");
-		lbl_genero.setBounds(10, 53, 70, 14);
-		panel_detalles.add(lbl_genero);
-		
-		JLabel lbl_censura = new JLabel("MAYORES DE 18");
-		lbl_censura.setBounds(10, 82, 101, 14);
-		panel_detalles.add(lbl_censura);
-		
-		JLabel lbl_sinapsis = new JLabel("SINAPSIS");
-		lbl_sinapsis.setBounds(10, 120, 70, 14);
-		panel_detalles.add(lbl_sinapsis);
-		
-		txt_nombre = new JTextField();
-		txt_nombre.setEditable(false);
-		txt_nombre.setColumns(10);
-		txt_nombre.setBounds(121, 25, 270, 20);
-		panel_detalles.add(txt_nombre);
-		
-		txt_genero = new JTextField();
-		txt_genero.setEditable(false);
-		txt_genero.setColumns(10);
-		txt_genero.setBounds(121, 53, 270, 20);
-		panel_detalles.add(txt_genero);
-		
-		txt_censura = new JTextField();
-		txt_censura.setEditable(false);
-		txt_censura.setColumns(10);
-		txt_censura.setBounds(121, 79, 270, 20);
-		panel_detalles.add(txt_censura);
-		
-		txtA_sinapsis = new JTextArea();
-		txtA_sinapsis.setEditable(false);
-		txtA_sinapsis.setBounds(121, 115, 270, 36);
-		panel_detalles.add(txtA_sinapsis);
 		
 		JButton btn_menu = new JButton("MENU");
 		btn_menu.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				panel_teatro.setVisible(false);
-				panel_detalles.setVisible(false);
+			//	panel_detalles.setVisible(false);
 				panel_menu= new panel_menu(ARCANE);
 				panel_menu.setBounds(0, 0, 1024, 800);
 				add(panel_menu);
@@ -115,14 +73,58 @@ public class panel_teatro extends JPanel {
 			public void actionPerformed(ActionEvent e) {
 				
 				panel_teatro.setVisible(false);
-				panel_detalles.setVisible(false);
+			//	panel_detalles.setVisible(false);
 				panel_datos= new panel_compra(peliculas,comboBox.getSelectedItem().toString(),"teatro",ARCANE);
-				panel_datos.setBounds(10, 11, 439, 468);
+				panel_datos.setBounds(0, 0, 1024, 800);
 				add(panel_datos);
 			}
 		});
 		btn_comprar.setBounds(121, 64, 106, 23);
 		panel_teatro.add(btn_comprar);
+		
+		JLabel lbl_nombre = new JLabel("NOMBRE");
+		lbl_nombre.setBounds(10, 117, 70, 14);
+		panel_teatro.add(lbl_nombre);
+		
+		txt_nombre = new JTextField();
+		txt_nombre.setEditable(false);
+		txt_nombre.setColumns(10);
+		txt_nombre.setBounds(121, 114, 270, 20);
+		panel_teatro.add(txt_nombre);
+		
+		JLabel lbl_genero = new JLabel("GENERO");
+		lbl_genero.setBounds(10, 142, 70, 14);
+		panel_teatro.add(lbl_genero);
+		
+		txt_genero = new JTextField();
+		txt_genero.setEditable(false);
+		txt_genero.setColumns(10);
+		txt_genero.setBounds(121, 142, 270, 20);
+		panel_teatro.add(txt_genero);
+		
+		JLabel lbl_censura = new JLabel("MAYORES DE 18");
+		lbl_censura.setBounds(10, 171, 101, 14);
+		panel_teatro.add(lbl_censura);
+		
+		txt_censura = new JTextField();
+		txt_censura.setEditable(false);
+		txt_censura.setColumns(10);
+		txt_censura.setBounds(121, 168, 270, 20);
+		panel_teatro.add(txt_censura);
+		
+		JLabel lbl_sinapsis = new JLabel("SINAPSIS");
+		lbl_sinapsis.setBounds(10, 209, 70, 14);
+		panel_teatro.add(lbl_sinapsis);
+		
+		txtA_sinapsis = new JTextArea();
+		txtA_sinapsis.setEditable(false);
+		txtA_sinapsis.setBounds(121, 204, 270, 36);
+		panel_teatro.add(txtA_sinapsis);
+		
+		lbl_fotos = new JLabel("");
+		lbl_fotos.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_fotos.setBounds(167, 285, 128, 167);
+		panel_teatro.add(lbl_fotos);
 		
 		t_pc= new logica_negocio_panelTeatro(this, peliculas,ARCANE);
 		
